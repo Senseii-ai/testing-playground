671e6fc528e73e36ac922ab27ad6ec13c98fc73f
prateek041 <prateeksingh9741@gmail.com>
2024-01-09 22:04:10 +0530

    Successfully deployed on Kubernetes
    Signed-off-by: prateek041 <prateeksingh9741@gmail.com>

--- microservices/python/app.py
+++ microservices/python/app.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
 import requests
+import os
 
 app = FastAPI()
+
+node_service_url = os.getenv("NODE_SERVICE_URL")
 
 
 @app.get("/")
 def test_root():
-    response = requests.get("http://localhost:9090/")
+    response = requests.get(f"{node_service_url}/")
     data = response.json()
     return data
