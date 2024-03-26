a97f0afa63f69b408bb427a903d9e0c627bf9ea1
prateek041 <prateeksingh9741@gmail.com>
2024-03-26 08:55:45 +0530

    langchain testing

--- image-gen/index.js
+++ image-gen/index.js
@@ -1,5 +1,18 @@
-import {openai} from "openai"
-import * as dotenv from "dotenv"
+const fs = require('fs');
+const OpenAI = require('openai');
+require('dotenv').config();
 
-dotenv.config()
-const openai = new openai
+const apiKey = process.env.OPENAI_API_KEY;
+
+const openai = new OpenAI({ apiKey: apiKey });
+
+async function main() {
+  const image = await openai.images.generate({
+    model: 'dall-e-3',
+    prompt:
+      'A banner image that is made up using repeating patterns and the color should be shades of neon purple lights. the pattern should be minimalist but concentrated with details and considering that I am a software engineer',
+  });
+
+  console.log(image.data);
+}
+main();
